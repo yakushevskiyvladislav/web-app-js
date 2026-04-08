243d079c8d7f8eaf248ab1386a5abf67001cf289
Vlad <130458321+yakushevskiyvladislav@users.noreply.github.com>
2026-04-08 09:15:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,131 @@
+//Лабораторная работа N2
+Задание 2.1
+const appConfig = {
+appTitle: 'Система учёта отзывов',
+defaultStatus: 'new',
+minValueForFilter: 800
+};
+let actionCount = 0;
+actionCount += 1;
+actionCount++;
+actionCount = actionCount + 1;
+appConfig.minValueForFilter = 1000;
+console.log(actionCount); 
+console.log(appConfig);
+
+Задание 2.2
+const task1 = {
+  id: 1,
+  title: 'пользователь',
+  value: 'text',
+  status: 'new',
+  createdAt: '2026-02-10'
+};
+const task2 = {
+  id: 2,
+  title: 'обьект отзыва',
+  value: 'text',
+  status: 'new',
+  createdAt: '2026-02-12'
+};
+const task3 = {
+  id: 3,
+  title: 'отзыв',
+  value: 'text',
+  status: 'new',
+  createdAt: '2026-02-14'
+};
+const task4 = {
+  id: 4,
+  title: 'комментарий',
+  value: 'text',
+  status: 'new',
+  createdAt: '2026-02-15'
+};
+const task5 = {
+  id: 5,
+  title: 'модерация',
+  value: 'text',
+  status: 'proccess',
+  createdAt: '2026-02-16'
+};
+const task6 = {
+  id: 6,
+  title: 'статистика',
+  value: 'text',
+  status: 'completed',
+  createdAt: '2026-02-17'
+};
+console.log(task1,task2,task3,task4,task5,task6)
+
+
+
+Задание 2.3
+const inputMinValue = "800";
+
+const minValue = Number(inputMinValue);
+if (Number.isNaN(minValue)) {
+  console.log("Ошибка: введённое значение не является числом.");
+} else {
+  console.log(minValue);
+}
+
+Задание 2.4
+const userAge = 19;
+const isBlocked = false;
+const hasAccess = userAge >= 18 && userAge < 65 && !isBlocked;
+console.log(hasAccess);
+
+Задание 2.5
+const task = {
+  id: 1,
+  title: 'Пользователь',
+  value: 1200,
+  status: 'new',
+  createdAt: '2026-02-10'
+};
+let statusDescription;
+switch (task.status) {
+  case 'new':
+    statusDescription = 'Новая запись';
+    break;
+  case 'done':
+    statusDescription = 'Завершено';
+    break;
+  default:
+    statusDescription = 'Неизвестный статус';
+    break;
+}
+let valueCategory;
+if (task.value >= 1000) {
+  valueCategory = 'Высокое значение';
+} else if (task.value >= 700) {
+  valueCategory = 'Среднее значение';
+} else {
+  valueCategory = 'Низкое значение';
+}
+console.log('Статус:', statusDescription);
+console.log('Категория по значению:', valueCategory);
+
+Задание 2.6
+const entities = [
+  { id: 1, status: "new" },
+  { id: 2, status: "processed" },
+  { id: 3, status: "new" },
+  { id: 4, status: "completed" },
+  { id: 5, status: "new" }
+];
+let newCount = 0;
+let i = 0;
+
+while (i < entities.length) {
+  if (entities[i].status === "new") {
+    newCount++;
+  }
+  i++; 
+}
+console.log(`Количество сущностей со статусом "new": ${newCount}`);
+
 const reviews = [
   { id: 1, text: «Отличное место!", value: 5, status: "new" },
   { id: 2, text: "Среднее качество обслуживания", value: 3, status: "processed" },
