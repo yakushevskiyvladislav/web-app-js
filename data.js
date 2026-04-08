a89bc9c93dd5d785a452f21443948687767f7bb0
Vlad <130458321+yakushevskiyvladislav@users.noreply.github.com>
2026-04-08 21:15:09 +0300

    Update data.js

--- data.js
+++ data.js
@@ -4,7 +4,8 @@ const reviews = [
   { id: 3, text: "Всё понравилось", value: 5, status: "old", createdAt: "2026-02-15"},
   { id: 4, text: "Неплохо, но можно лучше", value: 4, status: "new", createdAt: "2026-03-20"},
   { id: 5, text: "Ужасное обслуживание", value: 1, status: "old", createdAt: "2026-01-15" },
-  { id: 6, text: "Плоховато", value: 2, status: "new", createdAt: "2026-03-15"}
-
+  { id: 6, text: "Плоховато", value: 2, status: "new", createdAt: '2026-03-15'},
+  { id: 7, text: "Хорошее заведение", value: 4, status: "new", createdAt: "2026-03-15"},
+  { id: 8, text: "Рекомендую", value: 5, status: "new", createdAt: "2026-03-17"},
+  { id: 9, text: "Отвратительное обслуживание", value: 2, status: "new", createdAt: "2026-03-14"}
 ];
-
